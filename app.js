6e2ae34bf115266c893bed4d11090c126ee31645
aliyesinembattal <aliyeesnm@gmail.com>
2021-01-17 06:38:31 +0300

    Fixed UI and added dolar icon.

--- app.js
+++ app.js
@@ -39,9 +39,11 @@ function calculateResults() {
   const monthly = (principal * x * calculatedInterest) / (x - 1);
 
   if (isFinite(monthly)) {
-    monthlyPayment.value = monthly.toFixed(3);
-    totalPayment.value = (monthly * calculatedPayments).toFixed(3);
-    totalInterest.value = (monthly * calculatedPayments - principal).toFixed(3);
+    monthlyPayment.value = monthly.toFixed(3).toString() + " $";
+    totalPayment.value =
+      (monthly * calculatedPayments).toFixed(3).toString() + " $";
+    totalInterest.value =
+      (monthly * calculatedPayments - principal).toFixed(3).toString() + " $";
 
     //Show result
     results.style.display = "block";
